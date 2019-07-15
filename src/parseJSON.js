a082ac54424d7091324fdae179b3762942b785c4
omega1984 <omegareborn2019@gmail.com>
2019-07-15 16:39:25 -0700

    work on parseJSON (commited by Toby)

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,59 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  //declare variable for current character
+  let currentIndex = 0;
+  let currentCharacter = json[currentIndex];
+  //declare next character function
+  let next = function() {
+    currentIndex++;
+    currentCharacter = json[currentIndex]
+  }
+  //parseArray function
+
+  let parseArray = function() {
+    let result = [];
+    if (currentCharacter === ']') {
+      return result;
+    } else {
+      while(currentCharacter !== ']') {
+        result.push(parseElement());
+      }
+    }
+    return result;
+  }
+  function parseElement() {
+    if (currentCharacter = '"') {
+      next();
+      return parseString();
+    }
+  }
+
+  //parseObject function
+  let parseObj = function(){
+    let result = {};
+    if (currentCharacter === '}'){
+      return result;
+    }
+  }
+
+  if (currentCharacter === '{'){
+    next();
+    return parseObj();
+  }
+  //parseValue f
+  function parseString() {
+    let string = '';
+    while (currentCharacter !== '"') {
+      string += currentCharacter;
+      next();
+    }
+    next();
+    return string;
+  }
+
+  if (currentCharacter === '[') {
+    next();
+    return parseArray();
+  }
 };
