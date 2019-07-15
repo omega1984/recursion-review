70033a9dfce007368931d3c6750b5608db2f25b2
omega1984 <omegareborn2019@gmail.com>
2019-07-15 15:30:54 -0700

    stringifyJSON finished, all tests passed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) return "null";
+  if (Array.isArray(obj)){
+    var result = [];
+    for (var i = 0; i < obj.length; i++){
+      result.push(stringifyJSON(obj[i]));
+    }
+    return "[" + result.join(",") + "]";
+  }
+  if (typeof obj === "object"){
+    var result = [];
+    for (var key in obj){
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return "{" + result.join(",") + "}";
+  }
+  if (typeof obj === "number" || typeof obj === "boolean"){
+    return "" + obj;
+  }
+  return '"' + obj + '"';
 };
